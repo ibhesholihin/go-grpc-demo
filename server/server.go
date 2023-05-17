780714210f8494e7190edd0b335f0d0a9b403280
ibhesholihin <ibhe07@icloud.com>
2023-05-17 18:17:09 +0800

    server stream revision

--- server/server.go
+++ server/server.go
@@ -17,13 +17,18 @@ type helloServer struct {
 }
 
 func main() {
+	//listen on the port
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to start server %v", err)
 	}
+	// create a new gRPC server
 	grpcServer := grpc.NewServer()
+	// register the greet service
 	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("Server started at %v", lis.Addr())
+	//list is the port, the grpc server needs to start there
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to start server %v", err)
+		log.Fatalf("Failed to start: %v", err)
 	}
 }
